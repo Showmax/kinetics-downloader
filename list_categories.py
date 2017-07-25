cad234463b44182f703b96ca0dee3d7cb9cd4bd2
Ondrej Biza <ondrej.biza@gmail.com>
2017-07-25 19:23:07 +0200

    list classes improved

--- list_categories.py
+++ list_categories.py
@@ -1,12 +1,22 @@
-import json
+import argparse, json
 
 import lib.config as config
 
-with open(config.CATEGORIES_PATH, "r") as file:
-  categories = json.load(file)
+def main(args):
+  with open(config.CATEGORIES_PATH, "r") as file:
+    categories = json.load(file)
 
-for key in categories.keys():
-  print(key)
+  for key in categories.keys():
+    print(key)
 
-  for label in categories[key]:
-    print("\t{}".format(label))
+    if args.classes:
+      for label in categories[key]:
+        print("\t{}".format(label))
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("-c", "--classes", help="list classes for each category", action="store_true", default=False)
+
+  parsed = parser.parse_args()
+  main(parsed)
