b1614ccb01d188ced62f8260d030b17b95d3fcce
Ondrej Biza <ondrej.biza@gmail.com>
2017-11-30 20:02:03 +0100

    sound to tfrecord

--- sound_to_tfrecord.py
+++ sound_to_tfrecord.py
@@ -1,21 +1,50 @@
-import librosa
+import os, librosa
+import tensorflow as tf
 
 import lib.config as config
 import lib.utils as utils
 
 kinetics_sound_400_train = utils.load_json("resources/kinetics_full_sound_400_train.json")
 kinetics_sound_400_valid = utils.load_json("resources/kinetics_full_sound_400_val.json")
+kinetics_classes = utils.load_json("resources/kinetics_full_sound_400_classes.json")
 sampling_rate = 22050
 
 def load_audio(path):
   audio, _ = librosa.load(path, sr=sampling_rate, mono=True)
   return audio
 
-def convert_to_tfrecord(meta):
+def bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-  for path, cls in meta.items():
+def int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-  	audio = load_audio(path + ".mp3")
-  	print(audio.shape)
+def convert_to_tfrecord(meta, classes, root, records_path):
 
-convert_to_tfrecord(kinetics_sound_400_train)
+  writer = tf.python_io.TFRecordWriter(records_path)
+
+  i = 0
+  for path, cls_name in meta.items():
+
+    if i > 0 and i % 100 == 0:
+      print(i)
+
+    cls_id = classes[cls_name]
+
+    audio = load_audio(os.path.join(root, cls_name.replace(" ", "_"), path + ".mp3"))
+    audio_raw = audio.tostring()
+
+    length = audio.shape[0]
+
+    example = tf.train.Example(features=tf.train.Features(feature={
+      "length": int64_feature(length),
+      "sound_raw": bytes_feature(audio_raw),
+      "cls_id": int64_feature(cls_id)}))
+
+    writer.write(example.SerializeToString())
+    i += 1
+
+  writer.close()
+
+convert_to_tfrecord(kinetics_sound_400_train, kinetics_classes, config.TRAIN_SOUND_ROOT, "dataset/kinetics_full_sound_400_train.tfrecords")
+convert_to_tfrecord(kinetics_sound_400_valid, kinetics_classes, config.VALID_SOUND_ROOT, "dataset/kinetics_full_sound_400_val.tfrecords")
