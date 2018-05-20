e4d01fa04709be48e7b915162697e19d88f249a8
Ondrej Biza <ondrej.biza@gmail.com>
2018-05-20 13:10:39 +0200

    added create metadata from list

--- create_meta.py
+++ create_meta.py
@@ -1,142 +1,43 @@
-import argparse, os, random
+import argparse, random
 
 import lib.config as config
 import lib.constants as constants
+import lib.metadata as metadata
 import lib.utils as utils
 
-def get_valid_videos(videos, root, class_dirs=True):
-  """
-  Go through a list of videos and find all downloaded videos.
-  :param videos:        The list of video metadata.
-  :param root:          Videos root.
-  :param class_dirs:    Expect videos to be located in folders named after their classes (e.g. jogging/0123.mp4).
-  :return:              List of valid video ids for each class.
-  """
-
-  valid_videos = {}
-
-  for video_id in videos.keys():
-
-    if class_dirs:
-      cls = videos[video_id]["annotations"]["label"]
-      cls_path = cls.replace(" ", "_")
-      video_path = os.path.join(root, cls_path, video_id + ".mp4")
-    else:
-      cls = ""
-      video_path = os.path.join(root, video_id + ".mp4")
-
-    if os.path.isfile(video_path):
-      if cls in valid_videos:
-        valid_videos[cls].append(video_id)
-      else:
-        valid_videos[cls] = [video_id]
-
-  return valid_videos
-
-def get_valid_frames(videos, root, class_dirs=True):
-  """
-  Go through a list of videos and find all downloaded video frames.
-  :param videos:        The list of video metadata.
-  :param root:          Video frames root.
-  :param class_dirs:    Expect frames to be located in folders named after their classes
-                        (e.g. jogging/0123/frame0.jpg ...).
-  :return:              List of valid video ids for each class.
-  """
-
-  valid_videos = {}
-
-  for video_id in videos.keys():
-    if class_dirs:
-      cls = videos[video_id]["annotations"]["label"]
-      cls_path = cls.replace(" ", "_")
-      video_path = os.path.join(root, cls_path, video_id)
-    else:
-      cls = ""
-      video_path = os.path.join(root, video_id)
-
-    if os.path.isdir(video_path):
-      if cls in valid_videos:
-        valid_videos[cls].append(video_id)
-      else:
-        valid_videos[cls] = [video_id]
-
-  return valid_videos
-
-def get_valid_sound(videos, root, class_dirs=True):
-  """
-  Go through a list of videos and find all downloaded video sound tracks.
-  :param videos:    The list of video metadata.
-  :param root:          Video sounds root.
-  :param class_dirs:    Expect sounds to be located in folders named after their classes (e.g. jogging/0123.mp3).
-  :return:              List of valid video ids for each class.
-  """
-
-  valid_videos = {}
-
-  for video_id in videos.keys():
-    if class_dirs:
-      cls = videos[video_id]["annotations"]["label"]
-      cls_path = cls.replace(" ", "_")
-      video_path = os.path.join(root, cls_path, "{}.mp3".format(video_id))
-    else:
-      cls = ""
-      video_path = os.path.join(root, "{}.mp3".format(video_id))
-
-    if os.path.isfile(video_path):
-      if cls in valid_videos:
-        valid_videos[cls].append(video_id)
-      else:
-        valid_videos[cls] = [video_id]
-
-  return valid_videos
-
-def class_keys_to_video_id_keys(videos):
-  """
-  Transform a dictionary with keys = classes, values = video lists to a dictionary where key = video id, value = class.
-  :param videos:    Dictionary with classes as keys.
-  :return:          Dictionary with video ids as keys.
-  """
-
-  new_videos = {}
-
-  for cls, vids in videos.items():
-    for vid in vids:
-      new_videos[vid] = cls
-
-  return new_videos
 
 def main(args):
 
   # load and validate training videos
   videos = utils.load_json(config.TRAIN_METADATA_PATH)
   if args.format == constants.FORMAT_VIDEOS:
-    train_videos = get_valid_videos(videos, config.TRAIN_ROOT)
+    train_videos = metadata.get_valid_videos(videos, config.TRAIN_ROOT)
   elif args.format == constants.FORMAT_FRAMES:
-    train_videos = get_valid_frames(videos, config.TRAIN_FRAMES_ROOT)
+    train_videos = metadata.get_valid_frames(videos, config.TRAIN_FRAMES_ROOT)
   elif args.format == constants.FORMAT_SOUND:
-    train_videos = get_valid_sound(videos, config.TRAIN_SOUND_ROOT)
+    train_videos = metadata.get_valid_sound(videos, config.TRAIN_SOUND_ROOT)
   else:
     raise ValueError("Invalid format type.")
 
   # load and validate validation videos
   videos = utils.load_json(config.VAL_METADATA_PATH)
   if args.format == constants.FORMAT_VIDEOS:
-    validation_videos = get_valid_videos(videos, config.VALID_ROOT)
+    validation_videos = metadata.get_valid_videos(videos, config.VALID_ROOT)
   elif args.format == constants.FORMAT_FRAMES:
-    validation_videos = get_valid_frames(videos, config.VALID_FRAMES_ROOT)
+    validation_videos = metadata.get_valid_frames(videos, config.VALID_FRAMES_ROOT)
   elif args.format == constants.FORMAT_SOUND:
-    validation_videos = get_valid_sound(videos, config.VALID_SOUND_ROOT)
+    validation_videos = metadata.get_valid_sound(videos, config.VALID_SOUND_ROOT)
   else:
     raise ValueError("Invalid format type.")
 
   # load and validate test videos
   videos = utils.load_json(config.TEST_METADATA_PATH)
   if args.format == constants.FORMAT_VIDEOS:
-    test_videos = get_valid_videos(videos, config.TEST_ROOT, class_dirs=False)
+    test_videos = metadata.get_valid_videos(videos, config.TEST_ROOT, class_dirs=False)
   elif args.format == constants.FORMAT_FRAMES:
-    test_videos = get_valid_frames(videos, config.TEST_FRAMES_ROOT, class_dirs=False)
+    test_videos = metadata.get_valid_frames(videos, config.TEST_FRAMES_ROOT, class_dirs=False)
   elif args.format == constants.FORMAT_SOUND:
-    test_videos = get_valid_sound(videos, config.TEST_SOUND_ROOT, class_dirs=False)
+    test_videos = metadata.get_valid_sound(videos, config.TEST_SOUND_ROOT, class_dirs=False)
   else:
     raise ValueError("Invalid format type.")
 
@@ -156,9 +57,9 @@ def main(args):
     set_valid = {cls: videos for cls, videos in validation_videos.items() if cls in set_classes}
     set_test = test_videos
 
-    set_train = class_keys_to_video_id_keys(set_train)
-    set_valid = class_keys_to_video_id_keys(set_valid)
-    set_test = class_keys_to_video_id_keys(set_test)
+    set_train = metadata.class_keys_to_video_id_keys(set_train)
+    set_valid = metadata.class_keys_to_video_id_keys(set_valid)
+    set_test = metadata.class_keys_to_video_id_keys(set_test)
 
     set_classes = list(sorted(set_classes))
     set_classes = {cls: idx for idx, cls in enumerate(set_classes)}
@@ -182,18 +83,21 @@ def main(args):
     utils.save_json(test_path, dataset["test"])
     utils.save_json(classes_path, dataset["classes"])
 
-parser = argparse.ArgumentParser("Create metadata for all downloaded videos with the option to limit the number of "
-                                 "classes included.")
 
-parser.add_argument("format", help="{}, {} or {}".format(constants.FORMAT_VIDEOS, constants.FORMAT_FRAMES,
-                                                         constants.FORMAT_SOUND))
+if __name__ == "__main__":
 
-parser.add_argument("-s", "--sets", type=int, nargs="+", default=[400], help="list of sets to generate, each integer "
-                                                                             "denotes number of classes in the set")
-parser.add_argument("-f", "--force", default=False, action="store_true",
-                    help="create metadata even if training and validation sets do not contain the same classes")
+  parser = argparse.ArgumentParser("Create metadata for all downloaded videos with the option to limit the number of "
+                                   "classes included.")
 
-parser.add_argument("--save", default="resources/kinetics", help="save path")
+  parser.add_argument("format", help="{}, {} or {}".format(constants.FORMAT_VIDEOS, constants.FORMAT_FRAMES,
+                                                           constants.FORMAT_SOUND))
 
-parsed = parser.parse_args()
-main(parsed)
+  parser.add_argument("-s", "--sets", type=int, nargs="+", default=[400], help="list of sets to generate, each integer "
+                                                                               "denotes number of classes in the set")
+  parser.add_argument("-f", "--force", default=False, action="store_true",
+                      help="create metadata even if training and validation sets do not contain the same classes")
+
+  parser.add_argument("--save", default="resources/kinetics", help="save path")
+
+  parsed = parser.parse_args()
+  main(parsed)
